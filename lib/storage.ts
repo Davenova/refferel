428619a835640bb84242eda296596c8d25802304
Unicorn X <dudebrothers2@gmail.com>
2024-10-07 17:10:11 +0530

    Update storage.ts

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const url = 'mongodb+srv://nikandr:passwordtelegram@cluster0.m9vc0.mongodb.net/toolbox_data?retryWrites=true&w=majority&appName=Cluster0';
+const url = 'mongodb://localhost:27017';
 const dbName = 'referral-system';
 
 const client = new MongoClient(url);
@@ -9,17 +9,17 @@ const db = client.db(dbName);
 const referralsCollection = db.collection('referrals');
 const referredByCollection = db.collection('referredBy');
 
-export function saveReferral(userId: string, referrerId: string) {
-  referralsCollection.updateOne({ _id: referrerId }, { $push: { referrals: userId } }, { upsert: true });
-  referredByCollection.updateOne({ _id: userId }, { $set: { referrer: referrerId } }, { upsert: true });
+export async function saveReferral(userId: string, referrerId: string) {
+  await referralsCollection.updateOne({ _id: referrerId }, { $push: { referrals: userId } }, { upsert: true });
+  await referredByCollection.updateOne({ _id: userId }, { $set: { referrer: referrerId } }, { upsert: true });
 }
 
-export function getReferrals(userId: string): string[] {
+export async function getReferrals(userId: string): Promise<string[]> {
   const referrals = await referralsCollection.findOne({ _id: userId });
   return referrals?.referrals || [];
 }
 
-export function getReferrer(userId: string): string | null {
+export async function getReferrer(userId: string): Promise<string | null> {
   const referrer = await referredByCollection.findOne({ _id: userId });
   return referrer?.referrer || null;
 }
